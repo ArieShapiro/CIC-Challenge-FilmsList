a8e5f6370742bcf3f1456bdaa4bcbe08423e9c6c
Arie Shapiro <aryehshapiro@gmail.com>
2018-09-30 09:39:06 +0200

    filter is working

--- main.js
+++ main.js
@@ -1,26 +1,10 @@
 'use strict';
 
 (function () {
-
     // Load videos and render them on screen
-    // console.log(loadFilmList());
     loadFilmList()
 
 })();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -33,15 +17,56 @@ function renderFilmList(data) {
     data.forEach(film => {
         strHTML += `
             <li>
-                ${film.title}
-            </li>        
+                <h3>${film.title}</h3>
+                <p>${film.release_year}</p>
+                <p>${film.production_company}</p>
+            </li>                   
         `;
     });
     document.querySelector('.films-list').innerHTML = strHTML;
 }
 
+
+
 function loadFilmList() {
     axios.get('https://data.sfgov.org/resource/wwmu-gmzc.json').then(res => {
         renderFilmList(res.data);
     }).catch();
+}
+
+function onVideoSearch(ev) {
+
+    ev.preventDefault();
+
+    // Retrieve search term
+    var searchValue = document.querySelector('.search-box').value;
+
+    loadFilteredFilmList(searchValue);
+
+    // renderFilmList(data);
+}
+
+
+
+
+function loadFilteredFilmList(value) {
+
+    axios.get('https://data.sfgov.org/resource/wwmu-gmzc.json').then(res => {
+
+        var regex = new RegExp(value, "gi");
+        var searchValue = res.data.filter(film => {
+            return film.title.match(regex);
+        });
+         renderFilmList(searchValue);
+    }).catch();
+
+
+
+
+
+
+
+
+
+    // console.log(filmList[0])
 }
